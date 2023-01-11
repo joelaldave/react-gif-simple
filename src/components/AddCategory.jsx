c140339a05181bc70dc67b3775f9fe735f730457
Joel Aldave <aldavecueva@gmail.com>
2023-01-11 20:41:57 -0300

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+
+
+export const AddCategory = ({ OnNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) =>{
+        setInputValue(event.target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(inputValue.trim().length<=1) return;
+
+        OnNewCategory(inputValue.trim());
+
+        setInputValue('');
+    }
+
+  return (
+    <form onSubmit={ onSubmit} >
+        <input
+                type='text'
+                placeholder="Buscar Gifs"
+                value={inputValue}
+                onChange={onInputChange}
+        />
+    </form>
+    
+  )
+}
